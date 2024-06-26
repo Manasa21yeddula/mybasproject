42791d977fe6f44821152ff99e4be45fef1dfda5
MANASA YEDDULA <manasayeddula21@gmail.com>
2024-06-26 12:59:04 +0000

    initial Commit

---
+++ controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.sap.smarttable.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsap/smarttable/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
